ee96040a0708adb75a324ac40860d3c4a301897f
NusretC <nusretc@tuta.io>
2023-09-05 09:16:24 +0300

    v4.1

--- urfanatik2.py
+++ urfanatik2.py
@@ -183,9 +183,9 @@ if __name__ == "__main__":
     veritabani_olustur()
     while True:
         
-        #haber_cek_urfanatik()
+        haber_cek_urfanatik()
         #haber_cek_urfanatik_jandarma()
-        haber_cek_ajansurfa()
-        haber_cek_urfadasin()
+        #haber_cek_ajansurfa()
+        #haber_cek_urfadasin()
 
         time.sleep(60)
