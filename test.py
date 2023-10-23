95b7ba9992a0eb2bc0556d9baafda7ba24c6e393
NusretC <nusretc@tuta.io>
2023-10-23 23:36:04 +0300

    test v2 added

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import sqlite3
-import sys, os
+import sys,os
 
 def yeni_dosya_indir():
     url = "https://raw.githubusercontent.com/nusretcakir/urfanatik/main/urfanatik2.py"
@@ -14,17 +14,18 @@ def yeni_dosya_indir():
     open('v.txt', 'wb').write(r.content)
 
 def version_test():
-    # Programın yerel sürümü
-    with open("v.txt", "r") as f:
-        version = int(f.read())
 
-    # Programın net sürümü
+    #programin yerel surumu
+    with open("v.txt","r") as f:
+        version = int(f.read())  
+    
+    #programin net surumu
     url = "https://raw.githubusercontent.com/nusretcakir/urfanatik/main/v.txt"
-    new_version = int(str((BeautifulSoup(requests.get(url).content, "html.parser"))))
-
+    new_version = int(str((BeautifulSoup(requests.get(url).content,"html.parser"))))
+    
     if version != new_version:
         yeni_dosya_indir()
-        print("Program güncellendi, yeniden başlatılıyor...")
+        print("Program güncellendi,yeniden başlatılıyor...")
         os.execv(sys.executable, [sys.executable] + sys.argv)
         sys.exit()
 
@@ -33,8 +34,9 @@ def version_test():
 # 6343655697 fa chat id
 
 def gonder(text):
-    chat_ids = ["1571716280", ]
 
+    chat_ids = ["1571716280",]
+    
     api_url = f"https://api.telegram.org/bot6372109892:AAEhkH7mMGwbiLOXDKzgpazL2oc0iAnYs5k/sendMessage"
 
     for chat_id in chat_ids:
@@ -48,7 +50,7 @@ def veritabani_olustur():
     conn = sqlite3.connect("haberler.db")
     # Bir Cursor objesi oluştur
     cursor = conn.cursor()
-
+    
     # Haberler tablosunu oluştur
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS haberler (
@@ -57,21 +59,22 @@ def veritabani_olustur():
             url TEXT NOT NULL
         )
     """)
-
+    
     # Değişiklikleri kaydet ve bağlantıyı kapat
     conn.commit()
     conn.close()
 
 def haber_ekle(baslik, url):
+
     # Veritabanına bağlan
     conn = sqlite3.connect("haberler.db")
     # Bir Cursor objesi oluştur
     cursor = conn.cursor()
-
+    
     # URL'nin veritabanında zaten var olup olmadığını kontrol et
     cursor.execute("SELECT * FROM haberler WHERE url = ?", (url,))
     existing_news = cursor.fetchone()
-
+    
     if existing_news:
         print("Bu URL zaten veritabanında var.")
     else:
@@ -80,76 +83,145 @@ def haber_ekle(baslik, url):
         print("Haber başarıyla eklendi.")
         mesaj = baslik + "\n" + url
         gonder(mesaj)
-
+    
     # Değişiklikleri kaydet ve bağlantıyı kapat
     conn.commit()
     conn.close()
 
+
 def haber_cek_urfanatik():
+
     url = "https://www.urfanatik.com/haberleri/viransehir"
+
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
     headers = {
-        'User-Agent': user_agent
+    'User-Agent': user_agent
     }
-
-    response = requests.get(url, headers=headers)
+    
+    response = requests.get(url,headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
+
+    print("Sayfa okunamadi yada internet baglantisi kotu")
+    pass
+
 
     tum_divler = soup.find_all('div', class_='row c-nb c-nb-ms gut-1 gut-0-ms')
     for i in tum_divler:
+        
         a_href_etiketi = i.find('a', href=True)
-        url2 = "https://www.urfanatik.com" + a_href_etiketi['href']
+        url2 = "https://www.urfanatik.com"+a_href_etiketi['href']
 
         i = str(i.text.strip()).split("\n")
-
+        
         baslik = i[0]
         ozet = i[3]
         haber_ekle(baslik, url2)
+        
+
+
 
 def haber_cek_urfanatik_jandarma():
+
     url = "https://www.urfanatik.com/haberleri/jandarma"
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
     headers = {
-        'User-Agent': user_agent
+    'User-Agent': user_agent
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url,headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
+
+    print("Sayfa okunamadi yada internet baglantisi kotu")
+    pass
+
 
     tum_divler = soup.find_all('div', class_='row c-nb c-nb-ms gut-1 gut-0-ms')
     for i in tum_divler:
+        
         a_href_etiketi = i.find('a', href=True)
-        url2 = "https://www.urfanatik.com" + a_href_etiketi['href']
+        url2 = "https://www.urfanatik.com"+a_href_etiketi['href']
 
         i = str(i.text.strip()).split("\n")
-
+        
         baslik = i[0]
         ozet = i[3]
         haber_ekle(baslik, url2)
+            
+    
 
 def haber_cek_ajansurfa():
+
     url = "https://www.ajansurfa.com/haberleri/viransehir"
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
     headers = {
-        'User-Agent': user_agent
+    'User-Agent': user_agent
     }
+    try:
+        response = requests.get(url,headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+    except:
+        print("Sayfa okunamadi yada internet baglantisi kotu")
+        pass
 
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')
+    try:
+        tum_divler = soup.find_all('div', class_='row c-nb c-nb-ms gut-1 gut-0-ms')
+        for i in tum_divler:
+            
+            a_href_etiketi = i.find('a', href=True)
+            url2 = "https://www.ajansurfa.com"+a_href_etiketi['href']
 
-    tum_divler = soup.find_all('div', class_='row c-nb c-nb-ms gut-1 gut-0-ms')
-    for i in tum_divler:
-        a_href_etiketi = i.find('a', href=True)
-        url2 = "https://www.ajansurfa.com" + a_href_etiketi['href']
-
-        i = str(i.text.strip()).split("\n")
-
-        baslik = i[0]
-        ozet = i[3]
-        haber_ekle(baslik, url2)
+            i = str(i.text.strip()).split("\n")
+            
+            baslik = i[0]
+            ozet = i[3]
+            haber_ekle(baslik, url2)
+            
+   
+    except:
+        print("HTML parsta sıkıntı var")
+        pass
 
 def haber_cek_urfadasin():
-    url = "https://www.urfadasin.com/haberleri/viransehir
+
+    url = "https://www.urfadasin.com/haberleri/viransehir"
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+
+    headers = {
+    'User-Agent': user_agent
+    }
+    try:
+        response = requests.get(url,headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+    except:
+        print("Sayfa okunamadi yada internet baglantisi kotu")
+        pass
+
+    try:
+        tum_divler = soup.find_all('div', class_='card border-0 h-100 rounded-0')
+        for i in tum_divler:
+            
+            a_href_etiketi = i.find('a', href=True)
+            url2 = "https://www.urfadasin.com"+a_href_etiketi['href']
+
+            i = str(i.text.strip()).split("\n")
+
+            baslik = i[0]
+            haber_ekle(baslik, url2)
+                
+    except:
+        print("HTML parsta sıkıntı var")
+        pass
+
+if __name__ == "__main__":
+    veritabani_olustur()
+    while True:
+
+        haber_cek_urfanatik()
+        haber_cek_urfanatik_jandarma()
+        # haber_cek_ajansurfa()
+        # haber_cek_urfadasin()
+
+        time.sleep(60)
